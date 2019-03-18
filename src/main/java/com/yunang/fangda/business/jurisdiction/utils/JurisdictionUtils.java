97795dfb1ee28ba3090574f6202a6b054ff04471
shengzhizhe <15100162730@163.com>
2019-03-18 16:59:32 +0800

    123

--- src/main/java/com/yunang/fangda/business/jurisdiction/utils/JurisdictionUtils.java
+++ src/main/java/com/yunang/fangda/business/jurisdiction/utils/JurisdictionUtils.java
@@ -14,10 +14,10 @@ public class JurisdictionUtils {
 
     public List<JurisdictionModel> getDatas() {
         List<JurisdictionModel> list = new ArrayList<>();
-        list.add(new JurisdictionModel("1", "权限管理", "qxgl", "0", 0,null));
-        list.add(new JurisdictionModel("2", "部门管理", "bmgl", "0", 0,null));
-        list.add(new JurisdictionModel("3", "职位管理", "zwgl", "0", 0,null));
-        list.add(new JurisdictionModel("4", "职位管理", "", "0", 0,null));
+        list.add(new JurisdictionModel("1", "系统管理", "", "0", 0, null));
+        list.add(new JurisdictionModel("2", "权限管理", "qxgl", "1", 0, null));
+        list.add(new JurisdictionModel("3", "部门管理", "bmgl", "1", 0, null));
+        list.add(new JurisdictionModel("4", "职位管理", "zwgl", "1", 0, null));
 //        list.add(new JurisdictionModel("1", "账号管理", "account-zhgl", "0", 0));
 //        list.add(new JurisdictionModel("2", "新增账号", "account-save", "1", 1));
 //        list.add(new JurisdictionModel("3", "删除账号", "account-delete", "1", 1));
