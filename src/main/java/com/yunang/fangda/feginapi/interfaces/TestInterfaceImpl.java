498ee6ac641607109d3e4477b3d32a8ae43db4ad
shengzhizhe <15100162730@163.com>
2019-03-29 12:56:26 +0800

    添加fegin

--- src/main/java/com/yunang/fangda/feginapi/interfaces/TestInterfaceImpl.java
+++ src/main/java/com/yunang/fangda/feginapi/interfaces/TestInterfaceImpl.java
@@ -1,16 +1,12 @@
 package com.yunang.fangda.feginapi.interfaces;
 
-import com.yunang.fangda.feginapi.utils.FeginUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
 
 /**
  * @author: LD
@@ -19,48 +15,33 @@ import java.text.SimpleDateFormat;
  */
 public class TestInterfaceImpl {
 
-    @Autowired
-    private TestInterface testInterface = new FeginUtils().object.target(TestInterface.class, FeginUtils.URL);
-
-    public void test() {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
-        String stuno ="06038";
-        String time = "20170327152505";//simpleDateFormat.format(System.currentTimeMillis());
-        String stuname = "小高";
-//        String sign = "9C1FE04B8F6F268A92CD5FFE8B7E7A35";
-//        String md5Password = DigestUtils.md5DigestAsHex(model.getPassword().getBytes(StandardCharsets.UTF_8));
-        String str = stuname+"|"+stuno+"|"+time+"|"+"123456";
-        String s = DigestUtils.md5DigestAsHex(str.getBytes(StandardCharsets.UTF_8));
-        System.out.println(s);
-//        String s1 = s.toUpperCase();
-        String s2 = DigestUtils.md5DigestAsHex(s.getBytes(StandardCharsets.UTF_8));
-//        String sign = s2.toUpperCase();
-
-        System.out.println(s2);
-        Object object = testInterface.getOwner(time,s2,stuno,stuname);
-        System.out.println(object);
-    }
-
-    public void test2(){
-        Object weihuzu = testInterface.tst("weihuzu", "123456");
-        System.out.println(weihuzu.toString());
-    }
-
     public static void main(String[] args) {
-        String stuno ="06038";
-        String time = "20170327152505";//simpleDateFormat.format(System.currentTimeMillis());
+        String stuno = "06038";
+        String time = "20170327152505";
         String stuname = "小高";
-        String str = stuname+"|"+stuno+"|"+time+"|"+"123456";
-        String s = DigestUtils.md5DigestAsHex(str.getBytes(StandardCharsets.UTF_8));
-        System.out.println(s);
-        String s1 = s.toUpperCase();
-        String s2 = DigestUtils.md5DigestAsHex(s1.getBytes(StandardCharsets.UTF_8));
-        String sign = s2.toUpperCase();
-        String sign2 = "sign="+sign;
+        String str = stuname + "|" + stuno + "|" + time + "|" + "123456";
+        String str2 = "stuname=" + stuname + "&stuno=" + stuno + "&time=" + time + "&";
+        try {
+            String utils = utils(str2, str, "http://192.168.1.12/api/query/QUERY.asp");
+            System.out.println(utils);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param str  参数串(不包含加密) "time=" + time + "&" + "stuname=" + stuname + "&" + "stuno=" + stuno + "&"
+     * @param sign 需要加密的串明文 stuname + "|" + stuno + "|" + time + "|" + "123456"
+     * @param url1 请求地址 http://192.168.1.12/api/query/QUERY.asp
+     * @return
+     * @throws IOException
+     */
+    public static String utils(String str, String sign, String url1) throws IOException {
         URL url = null;
         HttpURLConnection httpurlconnection = null;
+        DataInputStream in = null;
         try {
-            url = new URL("http://192.168.1.12/api/query/QUERY.asp");
+            url = new URL(url1);
             httpurlconnection = (HttpURLConnection) url.openConnection();
             httpurlconnection.setDoInput(true);
             httpurlconnection.setDoOutput(true);
@@ -69,36 +50,50 @@ public class TestInterfaceImpl {
             httpurlconnection.setRequestProperty("Content-Type",
                     "application/x-www-form-urlencoded");
 
-            String s4 = "time="+time+"&"+"stuname="+stuname+"&"+"stuno="+stuno+"&"+sign2;
-            System.out.println(s4);
-            httpurlconnection.getOutputStream().write(s4.getBytes());
+            String sign1 = sign(sign);
+            String s = str + sign1;
+            httpurlconnection.getOutputStream().write(s.getBytes());
 
             httpurlconnection.getOutputStream().flush();
             httpurlconnection.getOutputStream().close();
             int code = httpurlconnection.getResponseCode();
-            System.out.println("code    " + code);
 
             if (code == 200) {
-
                 String cookie = httpurlconnection.getHeaderField("Set-Cookie ");
-                System.out.println(cookie);
                 // httpurlconnection.setRequestProperty( "Cookie", cookie);
 
-                DataInputStream in = new DataInputStream(httpurlconnection
+                in = new DataInputStream(httpurlconnection
                         .getInputStream());
                 int len = in.available();
                 byte[] by = new byte[len];
                 in.readFully(by);
-                String rev = new String(by);
-                System.out.println(rev);
-                in.close();
+                return new String(by);
+            } else {
+                return null;
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            return null;
         } finally {
+            if (in != null) {
+                in.close();
+            }
             if (httpurlconnection != null) {
                 httpurlconnection.disconnect();
             }
         }
+    }
+
+    /**
+     * 加密工具类
+     *
+     * @param str
+     * @return
+     */
+    private static String sign(String str) {
+        String s = DigestUtils.md5DigestAsHex(str.getBytes(StandardCharsets.UTF_8));
+        String s1 = s.toUpperCase();
+        String s2 = DigestUtils.md5DigestAsHex(s1.getBytes(StandardCharsets.UTF_8));
+        String sign = s2.toUpperCase();
+        return "sign=" + sign;
     }
 }
